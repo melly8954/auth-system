6f1ba92b5ac4d9aaffcd6204f17f1a05ae4d0f76
haily <49448330+melly8954@users.noreply.github.com>
2026-03-07 22:42:02 +0900

    feat: project init

---
+++ auth-session/src/main/java/com/biddingmate/authsession/AuthSessionApplication.java
@@ -0,0 +1,13 @@
+package com.biddingmate.authsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthSessionApplication.class, args);
+    }
+
+}
---
+++ auth-session/src/test/java/com/biddingmate/authsession/AuthSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biddingmate.authsession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSessionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
